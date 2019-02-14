8607d97e417c148db476084d45dc17c46450c97c
minhnt303 <minhnt303@gmail.com>
2019-02-15 03:13:12 +0700

    commit

--- js-practices/handout/Practice2/sortPractice.js
+++ js-practices/handout/Practice2/sortPractice.js
@@ -1,7 +1,17 @@
 'use strict'
 
 function sort(input) {
-  return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  //return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  for(let i = 0; i < input.length; i++){
+    for(let j = i + 1; j < input.length; j++){
+        if(input[i] > input[j]){
+            let temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
+        }
+    }
+}
+return input;
 }
 
 module.exports = sort
