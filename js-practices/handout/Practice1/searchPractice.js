502ea221a2eedb87b71c4e566ae221d021c1d497
minhnt303 <minhnt303@gmail.com>
2019-02-15 02:56:42 +0700

    commit

--- js-practices/handout/Practice1/searchPractice.js
+++ js-practices/handout/Practice1/searchPractice.js
@@ -1,7 +1,14 @@
 'use strict'
 
 function search(input, target) {
-  return  input.indexOf(target);  // Remove this line and change to your own algorithm
+  //return  input.indexOf(target);  // Remove this line and change to your own algorithm
+  for(let i = 0; i < input.length; i++){
+    if(input[i] == target){
+        //console.log(i);
+        return i;
+    } 
+  }
+  return -1;
 }
 
 module.exports = search
