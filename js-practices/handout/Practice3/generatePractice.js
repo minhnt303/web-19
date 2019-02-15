98ea90e71a609cfb414b3aa7cf18e599403babb3
minhnt303 <minhnt303@gmail.com>
2019-02-15 08:41:07 +0700

    commit

--- js-practices/handout/Practice3/generatePractice.js
+++ js-practices/handout/Practice3/generatePractice.js
@@ -1,6 +1,15 @@
 'use strict'
 
 function generate(testLengthArray){
+  for(let i = 0; i < testLengthArray.length; i++){
+    for(let j = i + 1; j < testLengthArray.length; j++){
+        if(testLengthArray[i] > testLengthArray[j]){
+            let temp = testLengthArray[i];
+            testLengthArray[i] = testLengthArray[j];
+            testLengthArray[j] = temp;
+        }
+    }
+  }
   return Array.from({length : testLengthArray.length})
     .map(item => ({
       input: Array.from({length: item}).map(item => []),
@@ -8,6 +17,8 @@ function generate(testLengthArray){
       output: -1
     })
   ); // Remove this line and change to your own algorithm
+
+    
 }
 
 module.exports = generate
