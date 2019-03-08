c6fb64c1fc1ec09cc2870d71cf811aea51ac7f1d
minhnt303 <minhnt303@gmail.com>
2019-03-08 16:16:47 +0700

    commit

--- minhnt303/server.js
+++ minhnt303/server.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/minhnt303',(err)=>{
 
 const server = express();
 
+
 server.use(express.static('public'));
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
