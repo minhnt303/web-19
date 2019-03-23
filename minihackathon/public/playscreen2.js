60d7c81be598cbec5a3e10195cbf1dff58f95506
minhnt303 <minhnt303@gmail.com>
2019-03-24 02:21:09 +0700

    commit

--- minihackathon/public/playscreen2.js
+++ minihackathon/public/playscreen2.js
@@ -31,6 +31,7 @@ $(document).ready(() => {
             document.getElementById('point-2').value = data.player2.point2[0];
             document.getElementById('point-3').value = data.player3.point3[0];
             document.getElementById('point-4').value = data.player4.point4[0];
+            
             }
             document.getElementById('player-1').innerText = data.player1.name1;
             document.getElementById('player-2').innerText = data.player2.name2;
