1d6218ea74b41171e424f8cbb0b437b7372eeb8b
minhnt303 <minhnt303@gmail.com>
2019-04-03 13:45:29 +0700

    commit

--- techkids-hotgirl/back-end/public/register.js
+++ techkids-hotgirl/back-end/public/register.js
@@ -1,1 +1,26 @@
-console.log('ajshdjsahdiasdju')
+window.onload = () => {
+    console.log("Register")
+    const form = document.getElementById("register-form");
+    form.addEventListener("submit", e => {
+        event.preventDefault();
+
+        console.log(form.inputEmail.value)
+
+        const loginInfo = { "email": form.inputEmail.value, "password": form.inputPassword.value };
+        console.log(loginInfo)
+        $.ajax({
+            type: "POST",
+            url: "/api/auth/register",
+            data: {
+                "email": form.inputEmail.value, "password": form.inputPassword.value, "facebookId": form.inputfbId.value, "firstName": form.inputFirstName.value, "lastName": form.inputLastName.value, "avatarUrl":form.inputavatarUrl.value, "permissions":form.inputpermissions.value
+            },
+            success: data => {
+                console.log(data + 'INFO')
+                window.alert(data.message);
+            },
+            error: error => {
+                console.log(error);
+            }
+        });
+    })
+};
